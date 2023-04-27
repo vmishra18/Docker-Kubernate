8fabd5c9bab94b349cc42adc1df5538624b85f26
Vishal Mishra <64347475+vmishra18@users.noreply.github.com>
2023-04-27 23:14:14 +0530

    Add files via upload

---
+++ spring-boot-docker/src/main/java/com/spring/boot/docker/SpringBootDockerApplication.java
@@ -0,0 +1,22 @@
+package com.spring.boot.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootDockerApplication {
+	
+	@GetMapping("/message")
+	public String getMessage() {
+		return "Running Successfully";
+		
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerApplication.class, args);
+	}
+
+}
